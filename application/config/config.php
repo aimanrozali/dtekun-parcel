f9dde383e504f3f21c5448b6e4c80cea5cb4bf33
aimanrozali <aimanrozali@student.usm.my>
2022-12-28 10:46:57 +0800

    config change

--- application/config/config.php
+++ application/config/config.php
@@ -23,11 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
-$config['base_url'] = 'http://$_SERVER["SERVER_ADDR"]/dtekun-parcel/';
-=======
 $config['base_url'] = 'http://localhost/dtekun-parcel/';
->>>>>>> 91304181cbebe22d48a24c849cbb76c5842110f9
 
 /*
 |--------------------------------------------------------------------------
