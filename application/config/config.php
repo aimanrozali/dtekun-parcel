d57ffa37854d5b6159e8b8c7f063ef187879b9b8
amirulirrfaan <74703771+amirulirrfaan@users.noreply.github.com>
2022-12-28 22:05:02 +0800

    localhost base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://34.175.235.111/dtekun-parcel/';
+$config['base_url'] = 'http://localhost/dtekun-parcel/';
 
 /*
 |--------------------------------------------------------------------------
