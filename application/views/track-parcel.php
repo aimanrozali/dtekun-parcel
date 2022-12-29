c90beb063bd08155ec263bb1faeb5495f0eaec11
Irdina <irdinairwan1@gmail.com>
2022-12-29 20:58:50 +0800

    update ui track-parcel Changes to be committed: modified:   application/views/track-parcel.php

--- application/views/track-parcel.php
+++ application/views/track-parcel.php
@@ -11,41 +11,32 @@
                       </form>
                     </nav>
                   </div>
-                </div>
-
-                <div class="card">
-                  <div class="card-header">
-                    <h4>Parcel Details</h4>
-                  </div>
+          
                 <div class="card-body">
                         <div class="table-responsive">
                           <table class="table table-striped" id="table-1">
                             <thead>
                               <tr>
-                                <th>Tracking Number</th>
-                                <th>Customer Name</th>
-                                <th>Courier Type</th>
-                                <th>Date Arrived</th>
-                                <th>Status</th>
-                                <th>Details</th>
-                              </tr>
+                                <th class="text-center"> Tracking Number </th>
+                                <th class="text-center"> Customer Name</th>
+                                <th class="text-center"> Courier Type</th>
+                                <th class="text-center"> Date Arrived</th>
+                                <th class="text-center"> Status</th>
+                            </tr>
                             </thead>
                             <tbody>
                               <tr>
                         
-                                <td>ABC123</td>
-                                <td>
+                              <td class="text-center"> ABC123</td>
+                              <td class="text-center">
                                   Amirul Irfan
                                 </td>
-                                <td>
+                                <td class="text-center">
                                   JNT
                                 </td>
-                                <td>2018-01-20</td>
-                                <td>
+                                <td class="text-center">2018-01-20</td>
+                                <td class="text-center">
                                   <div class="badge badge-success badge-shadow">Claimed</div>
-                                </td>
-                                <td>
-                                <a href="#" class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></a>
                                 </td>
 
                               </tr>
