49cdb7165d522634d4dbe374413c891a07baa887
aimanrozali <aimanrozali@student.usm.my>
2023-01-09 11:32:34 +0800

    add model

---
+++ application/models/ParcelModel.php
@@ -0,0 +1,19 @@
+<?php
+
+class ParcelModel extends CI_Model
+{
+  function __construct()
+  {
+    //Model Constructor
+    parent::__construct();
+  }
+
+  //List all queries in function
+
+  public function fetchAllParcel()
+  {
+    //Put queries Here
+  }
+}
+
+?>
