84c718c85d7c1a61787f538a69b2ad3b2aaa380b
aimanrozali <aimanrozali@student.usm.my>
2023-01-16 22:24:58 +0800

    ParcelModel db update

--- application/models/ParcelModel.php
+++ application/models/ParcelModel.php
@@ -1,33 +1,32 @@
 <?php
-defined ('BASEPATH') OR exit('No direct script access allowed. ');
+defined('BASEPATH') or exit('No direct script access allowed. ');
 
 class ParcelModel extends CI_Model
 {
-  public function __construct(){
-    $this->load->database();
-}
-
-public function get_parcel() {
-    $query = $this->db->get('parcel');
-    if($query->num_rows() > 0) {
-        $result = $query->result_array();
-        return $result;
-    }
-
-    else{
-        return false;
-    }
-}
-
-public function save($capsule)
-{
-    $insert = $this->db->insert('parcel', $capsule);
-
-    if($insert)
+    public function __construct()
     {
-       return $msg = ("Data Inserted Successfully");
+        $this->load->database();
     }
-}
+
+    public function get_parcel()
+    {
+        $query = $this->db->get('Parcel');
+        if ($query->num_rows() > 0) {
+            $result = $query->result_array();
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    public function save($capsule)
+    {
+        $insert = $this->db->insert('Parcel', $capsule);
+
+        if ($insert) {
+            return $msg = ("Data Inserted Successfully");
+        }
+    }
 
 
 }
