f71ac693baa9d6bab89cbb78d8583310bda7ea88
alyamzln <nur27alya@gmail.com>
2023-01-27 09:33:50 +0800

    update image on tracking page

--- application/views/track-parcel.php
+++ application/views/track-parcel.php
@@ -89,5 +89,67 @@
   </div>
   </div>
 
+  <div class="main-content">
+        <section class="section">
+          <div class="section-body">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="bootstrap snippet">
+                      <section id="portfolio" class="gray-bg padding-top-bottom">
+                        <div class="projects-container scrollimation in">
+                          <div class="row">
+                            <article class="col-md-4 col-sm-6 portfolio-item web-design apps psd">
+                              <div class="portfolio-thumb in">
+                                <a href="#" class="main-link">
+                                  <img class="img-responsive img-center img-box" src="<?php echo base_url(); ?>assets/img/parcel-1.jpeg" alt="Parcel 1">
+                                  <span class="project-title">How to claim your parcel</span>
+                                  <span class="overlay-mask"></span>
+                                </a>
+                                <a class="enlarge cboxElement" href="#" title="Bills Project"><i
+                                    class="fa fa-expand fa-fw"></i></a>
+                                <a class="link" href="#"><i class="fa fa-eye fa-fw"></i></a>
+                              </div>
+                            </article>
+                            <article class="col-md-4 col-sm-6 portfolio-item apps">
+                              <div class="portfolio-thumb in">
+                                <a href="#" class="main-link">
+                                  <img class="img-responsive img-center img-box" src="<?php echo base_url(); ?>assets/img/parcel-2.jpeg" alt="Parcel 2">
+                                  <span class="project-title">Customer alert!</span>
+                                  <span class="overlay-mask"></span>
+                                </a>
+                                <a class="link centered" href="#"><i class="fa fa-eye fa-fw"></i></a>
+                              </div>
+                            </article>
+                            <article class="col-md-4 col-sm-6 portfolio-item web-design psd">
+                              <div class="portfolio-thumb in">
+                                <a href="#" class="main-link">
+                                  <img class="img-responsive img-center img-box" src="<?php echo base_url(); ?>assets/img/parcel-3.jpeg" alt="Parcel 3">
+                                  <span class="project-title">Price list</span>
+                                  <span class="overlay-mask"></span>
+                                </a>
+                                <a class="enlarge centered cboxElement" href="#" title="Get Colored"><i
+                                    class="fa fa-expand fa-fw"></i></a>
+                              </div>
+                            </article>
+                          </div>
+                        </div>
+                      </section>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
 </section>
 
+<style>
+.img-box {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
